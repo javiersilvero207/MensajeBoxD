b36d5c1ebd0a28ed75969936f2c9c28c0c8d841c
Javiersilvero207 <javiersilvero207@gmail.com>
2025-01-28 20:15:08 +0100

    Proyecto MensajeBox

--- MensajeBoxDefinitivo/MainWindow.xaml.cs
+++ MensajeBoxDefinitivo/MainWindow.xaml.cs
@@ -1,28 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace MensajeBoxDefinitivo
+namespace Sergioteacher.Csharp04.MensajeBox
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                OpenFileDialog fichero = new OpenFileDialog();
+                if (fichero.ShowDialog() == true)
+                {
+                    txtEditor.Text = File.ReadAllText(fichero.FileName);
+                }
+                else
+                {
+                    MessageBox.Show(" No se ha selecionado NADA", "Nada de nada", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+            }
+        }
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {{
+            SaveFileDialog ficherito = new SaveFileDialog();
+            ficherito.Filter = "Text file (*.txt)|*.txt|C# file (*.cs)|*.cs|All files (*.*)|*.*";
+            ficherito.InitialDirectory = @"c:\";
+            ficherito.AddExtension = true;
+            if (ficherito.ShowDialog() == true)
+            {
+                File.WriteAllText(ficherito.FileName, txtEditor.Text);
+            }
+            else
+            {
+                MessageBox.Show(" Cancelado ", "Na de na", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
         }
     }
 }
